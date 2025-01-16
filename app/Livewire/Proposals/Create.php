957716d56a2be003baebc8e9c7250478b549b3f0
Douglascrc <douglasbuscam@gmail.com>
2025-01-16 12:38:12 -0300

    save proposals

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -9,6 +9,42 @@ class Create extends Component
 {
     public Project $project;
 
+    public bool $modal = false;
+
+    public bool $agree = false;
+
+    public string $email = '';
+
+    public int $hours = 0;
+
+    public function validation() {
+
+    }
+
+    protected array $rules = [
+        'agree' => 'required|boolean',
+        'email' => 'required|email',
+        'hours' => 'required|numeric|min:1',
+    ];
+
+    public function save() {
+  
+        $this->validate();
+        
+        if(!$this-> agree) {
+            $this->addError('agree', 'Você precisa concordar com os termos de uso');
+            return;
+        }
+
+        $this->project->proposals()
+            ->updateOrcreate(
+                ['email' => $this->email],
+                ['hours' => $this->hours]
+            );
+
+        $this->modal = false;
+    }
+
     public function render()
     {
         return view('livewire.proposals.create');
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,6 +3,8 @@
     <div class="grid grid-cols-3 gap-6">
         <livewire:projects.show :project="$project" />
         <livewire:projects.proposals :project="$project" />
+
+        <livewire.proposals.create :project="$project" />
     </div>
     
 </x-layouts.app>
